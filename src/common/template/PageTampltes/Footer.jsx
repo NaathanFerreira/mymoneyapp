e79ac2167f4a0a95d6f8567dfb71a38bd0abdc71
Nathan Ferreira <nathan.ferreira@pca.com.br>
2021-03-18 19:09:25 -0300

    Footer criado

---
+++ src/common/template/PageTampltes/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default props => {
+    return (
+        <footer className="main-footer">
+            <strong>
+                Copyright &copy; 2021
+                <a href="https://nathan-ferreira-portfolio.netlify.app/" target="_blank"> Nathan</a>.
+            </strong>
+        </footer>
+    )
+}
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -2,12 +2,17 @@ import './App.css';
 
 import Header from '../common/template/PageTampltes/Header'
 import SideBar from '../common/template/PageTampltes/SideBar'
+import Footer from '../common/template/PageTampltes/Footer';
 
 export default props => {
   return (
     <div className="wrapper">
       <Header/>
       <SideBar/>
+      <div className="content-wrapper">
+        <h1>Conteúdo</h1>
+      </div>
+      <Footer/>
     </div>
   )
 }
