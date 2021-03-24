e214e5de3281bc8258f2e04e038aae9851c609b6
Nathan Ferreira <nathan.ferreira@pca.com.br>
2021-03-24 15:45:02 -0300

    TabHeader conectado com Redux

--- src/common/tab/TabHeader.jsx
+++ src/common/tab/TabHeader.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-export default props => {
+import { selectTab } from './tabActions'
+
+function TabHeader(props){
+
+    const selected = props.tab.selected === props.target
+
     return (
-        <li>
-            <a href="" data-toggle='tab' data-target={props.target}>
+        <li className={selected ? "active" : ''}>
+            <a href="javascript:;" data-toggle='tab' data-target={props.target}
+            onClick={() => props.selectTab(props.target)}>
                 <i className={`fa fa-${props.icon}`}></i> {props.label}
             </a>
         </li>
     )
 }
+
+const mapStateToProps = state => ({
+    tab: state.tab
+})
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TabHeader)
--- src/main/Reducers.js
+++ src/main/Reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 
 import DashboardReducer from '../dashboard/DashboardReducer'
+import TabReducer from '../common/tab/tabReducer'
 
 // estado geral da aplicação
 const rootReducer =  combineReducers({
-  dashboard: DashboardReducer
+  dashboard: DashboardReducer,
+  tab: TabReducer
 })
 
 export default rootReducer
