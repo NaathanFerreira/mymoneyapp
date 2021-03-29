f6fc8a8afe6e2e16667bcabef0cdc735528a9de0
Nathan Ferreira <nathan.ferreira@pca.com.br>
2021-03-29 16:45:02 -0300

    Componente de usuário logado criado(usuário temporário)

---
+++ src/auth/authReducer.js
@@ -0,0 +1,22 @@
+const userKey = "_mymoner_user"
+const INITIAL_STATE = {
+    user: {name: "Nathan", email: "nathan@hotmail.com"},//JSON.parser(localStorage.getItem(userKey)),
+    validToken: false
+}
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case "TOKEN_VALIDATED":
+            if (action.payload) {
+                return { ...state, validToken: true }
+            } else {
+                localStorage.removeItem(userKey)
+                return { ...state, validToken: false, user: null }
+            }
+        case "USER_FETCHED":
+            localStorage.setItem(userKey, JSON.stringify(action.payload))
+            return {...state, user: action.payload, validToken: true}
+        default:
+            return state
+    }
+}
